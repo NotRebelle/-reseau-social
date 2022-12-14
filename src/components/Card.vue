682a608de6c54fe17ff7dab1602697a1b390bed7
NotRebelle <cccoop.cc@gmail.com>
2022-12-14 21:07:02 +0100

    push final

---
+++ src/components/Card.vue
@@ -0,0 +1,205 @@
+<template>
+    <div id="card">
+        <div id="card-info">
+            <div id="card-info-user">
+                <img id="user-image" :src="data.usrImg" />
+                <h3 id="h3-title">{{ data.pseudo }}</h3>
+            </div>
+            <div id="card-info-date">
+                <h4 id="h4-info">{{ new Date(data.date) }}</h4>
+            </div>
+        </div>
+        <div id="card-image">
+            <img id="post-image" :src="data.urlImgArticle" />
+        </div>
+        <div id="card-description">
+            <p id="description">{{ data.contenu }}</p>
+        </div>
+        <div id="card-like">
+             
+        </div>
+        <div id="card-comments">
+            <div id="card-comments-title">
+                <h3 id="h3-title">Commentaires : </h3>
+            </div>
+            <Comment v-for="comment in data.commentaires" :key="comment.id" :data="comment"></Comment>
+        </div>
+        <div v-if="userStore.user.isLogged" id="card-comments-add">
+            <div id="card-comments-add-title">
+                <h3 id="h3-title">Ajouter un commentaire : </h3>
+            </div>
+            <form id="form-main">
+                <textarea id="form-textarea" rows="4" placeholder="Laissez un nouveau commentaire" ></textarea>
+                <input id="form-submit" type="submit" />
+            </form>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import Comment from './Comment.vue'
+    import {useUserStore} from "../stores/userStore"
+
+    const props = defineProps({
+        data: Object
+    })
+
+    const userStore = useUserStore()
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,100;1,500&display=swap');
+
+#card {
+    width: 50%;
+    height: auto;
+    border-radius: 10px;
+    background-color: #317AC1;
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 15px;
+}
+
+#card-info {
+    width: 100%;
+    height: 80px;
+    display: flex;
+    flex-direction: row;
+}
+
+#card-info-user {
+    width: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+#h3-title {
+    font-size: 30px;
+    font-weight: 600;
+    margin-left: 20px;
+}
+
+#h4-info {
+
+    margin-right: 20px;
+}
+
+#user-image {
+    width: 45px;
+    height: 45px;
+    margin-left: 10px;
+}
+
+#card-info-date {
+    width: 60%;
+    height: 100%;
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+   
+}
+#card-info-date h4{
+    font-size: 1.2rem !important;
+}
+
+#card-image {
+    width: 50%;
+    height: 350px;
+    align-items: center;
+    position:relative;
+    margin: auto;
+    
+}
+
+#post-image {
+    width: 100%;
+    height: 100%;
+}
+
+#card-description {
+    width: 100%;
+    height: auto;
+}
+
+#description {
+
+    font-size: 20px;
+    padding-left: 35px;
+   
+}
+
+#card-counters {
+    width: 100%;
+    height: 50px;
+    background-color: #E8E8E8;
+}
+
+#card-comments {
+    width: 100%;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+}
+
+#card-comments-title {
+    width: 100%;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid black;
+}
+
+#card-comments-add {
+    width: 100%;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+}
+
+#card-comments-add-title {
+    width: 100%;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid black;
+}
+
+#form-main {
+    width: 100%;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 40px;
+}
+
+#form-textarea {
+    width: 97%;
+    height: 140px;
+    margin-bottom: 30px;
+    border-radius: 5px;
+    border: 1px solid grey;
+    padding-left: 10px;
+    font-size: 15px;
+}
+
+#form-submit {
+    width: 20%;
+    height: 35px;
+    border-radius: 5px;
+    border: 1px solid black;
+    font-size: 15px;
+   
+    background-color: #E1A624;
+    color: white;
+    cursor: pointer;
+    margin-bottom: 20px;
+}
+*{
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+    <header id="header">
+        <div id="left">
+            <img id="img-vue" src="../assets/images/vue.png" />
+            <h1>WebBook</h1>
+            <h4></h4>
+           
+        </div>
+        <div id="right">
+            <h2>Le réseau social des développeurs web</h2>
+        </div>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+#header {
+    width: 100%;
+    height: 10vh;
+    background-color: #317AC1;
+    display: flex;
+    flex-direction: row;
+}
+
+#left {
+    width: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding-left: 30px;
+    color: white
+}
+
+#right {
+    width: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+    padding-right: 20px;
+    color: white;
+    text-transform: uppercase;
+}
+
+
+
+#img-vue {
+    width: 50px;
+    height: 50px;
+    margin-right: 10px;
+ 
+
+}
+
+</style>
